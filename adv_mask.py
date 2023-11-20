88ea33e6ae6741addad5c108cb352b7e8c49ff98
Suraj <surajsemailid@gmail.com>
2023-11-19 19:21:25 -0500

    adv mask

--- adv_mask.py
+++ adv_mask.py
@@ -1,6 +1,5 @@
-## Erase and Check via L0 optimization
-## Method: Stochastic Relaxation via RelaxedBernoulli
-## argmax_{mask} loss(prompt \odot mask) s.t. has least possible sparsity 
+## Erase and Check via approximate L0 optimization
+## argmax_{mask} loss(prompt \odot mask) s.t. mak has least possible sparsity 
 
 ## Note: masking is performed by simply doing mask * prompt, as 
 ## [PAD] token is mapping to 0 anyways
@@ -9,22 +8,8 @@ import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 import argparse
 
-
-def projection(mask, adv_len=10, threshold = 0.5):
-    
-    # Find bottom k mask values
-    _, ind = torch.topk(mask, k = adv_len, largest=False)
-
-    # Find mask values less than a threshold
-    ind_thresh = mask > threshold
-
-    new_mask = torch.ones_like(mask)
-    new_mask[ind] = 0
-    new_mask[ind_thresh] = 1
-    return new_mask
-
 def adversarial_mask(input, model, tokenizer, word_embeddings,
-        num_adv=10, num_iters=50, init_temp=0.5, reg_const=0.1):
+        num_iters=50, init_temp=0.5, reg_const=0.1):
     """
     Find an adversarial mask for the safety classifier 
 
@@ -33,12 +18,11 @@ def adversarial_mask(input, model, tokenizer, word_embeddings,
         model: Safety classifier
         tokenizer: Tokenizer for the safety classifier
         word_embeddings: Embedding layer of the safety classifier
-        embedding_matrix: Embedding matrix of the safety classifier
-        num_adv: Number of adversarial tokens to append
         num_iters: Number of iterations
-        batch_size: Batch size
+        init_temp: Initial temperature value for RelaxedBernoulli
+        reg_const: Regularization constant enforcing masks to have small sparsity
     Returns:
-        adv_prompt: Input prompt with adversarial suffix
+        sampled_prompt: input prompt with learnt mask 
     """
     # Set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -47,57 +31,62 @@ def adversarial_mask(input, model, tokenizer, word_embeddings,
     tokens = torch.tensor(tokenizer.encode(input)).unsqueeze(0).to(device)
 
     # Initialize to all ones
-    mask_probs = torch.ones_like(tokens).float() * 0.
+    mask_probs = torch.zeros_like(tokens).float()
     mask_probs = torch.nn.Parameter(mask_probs.requires_grad_().to(device))
-    optimizer = torch.optim.SGD(params=[mask_probs], lr=1e-2, weight_decay=0.)
+    optimizer = torch.optim.SGD(params=[mask_probs], lr=1e-1, momentum=0.9, weight_decay=0.)
 
+    apply_mask_to_tokens = False
+    print_outputs = False
 
     for i in range(num_iters):
 
         optimizer.zero_grad()
 
         # Heuristic: Decay temperature over time
-        # as temp -> 0; RelaxedBernoulli -> Bernoulli
+        # as temp -> 0; mask becomes binary
         temp = init_temp / (i+1)
 
-        # RelaxedBernoulli distribution
-        binary_mask = torch.distributions.relaxed_bernoulli.RelaxedBernoulli(temperature=temp, 
-                                                                             probs=torch.sigmoid(mask_probs))
- 
+        sampled_mask = torch.sigmoid(mask_probs / temp)
+
         # Regularize mask such that most of the probabilities are close to one
         regularizer = (torch.sigmoid(mask_probs) - 1).pow(2).mean()
 
-        # Get token embeddings
-        embeddings = word_embeddings(tokens)
-
-        # Mask embeddings
-        sampled_mask = binary_mask.sample()
-        masked_embeddings = sampled_mask.unsqueeze(2) * embeddings
+        if apply_mask_to_tokens:
+            sampled_tokens = (sampled_mask * tokens).long()
+            embeddings = word_embeddings(sampled_tokens)
+        else:
+            embeddings = word_embeddings(tokens)
+            embeddings = sampled_mask.unsqueeze(2) * embeddings
         
         # Class 0 is harmful
-        output = model(inputs_embeds=masked_embeddings, labels=torch.tensor([0]).to(device)) 
+        output = model(inputs_embeds=embeddings, labels=torch.tensor([0]).to(device)) 
 
         loss = output.loss + reg_const * regularizer
         loss.backward() 
         optimizer.step()
 
-        if i % 10 == 0:
+        if apply_mask_to_tokens is False:
+            sampled_tokens = torch.round(sampled_mask * tokens).long()
+            sampled_prompt = tokenizer.decode((sampled_tokens)[0][1:-1])
+
+        if i % 10 == 0 and print_outputs:
             print("Iteration", i, " Loss: ",  loss.item(), 
                   " Output: ", output.loss.item(), 
                   " Reg: ", regularizer.item())
-
-            sampled_tokens = torch.round(sampled_mask * tokens).long()
-            sampled_prompt = tokenizer.decode((sampled_tokens)[0][1:-1])
+             
             print("Sampled Prompt: " + sampled_prompt)
+            print("Prediction: " + ("safe" if output.logits[0].argmax().item() == 1 else "harmful"))
+            print(output.logits[0])
+            print()
+
     
     return sampled_prompt
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Generate adversarial prompts using GCG.')
-    parser.add_argument('--num_adv', type=int, default=10, help='Number of adversarial tokens to append')
-    parser.add_argument('--prompts_file', type=str, default='data/harmful_prompts_test.txt', help='File containing prompts')
+    parser = argparse.ArgumentParser(description='Adversarial masks for the safety classifier.')
+    parser.add_argument('--prompts_file', type=str, default='data/adversarial_prompts_t_21.txt', help='File containing prompts')
 
     args = parser.parse_args()
 
@@ -109,12 +98,11 @@ if __name__ == '__main__':
     model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
 
     # Load model weights
-    #model_wt_path = 'models/distillbert_saved_weights.pt'
-    #model.load_state_dict(torch.load(model_wt_path))
+    model_wt_path = 'models/distillbert_saved_weights.pt'
+    model.load_state_dict(torch.load(model_wt_path))
     model.to(device)
     model.eval()
 
-    num_adv = args.num_adv
     prompts_file = args.prompts_file
 
     # Load prompts
@@ -127,16 +115,21 @@ if __name__ == '__main__':
 
     # Open file to write prompts
     for input in prompts:
-        print("ORG PROMPT: " + input)
+        #print("ORG PROMPT: " + input)
+        tokens = torch.tensor(tokenizer.encode(input)).unsqueeze(0).to(device)
+        model_output = model(tokens)
+        print("Original Prediction: " + ("safe" if model_output[0].argmax().item() == 1 else "harmful"))
+
+        word_embeddings = model.distilbert.embeddings.word_embeddings
 
         sampled_prompt = adversarial_mask(input, model, tokenizer, 
-                                      model.distilbert.embeddings.word_embeddings,
-                                      num_adv=num_adv,num_iters=1000,
-                                      init_temp=5, reg_const=1.0)
+                                      word_embeddings,
+                                      num_iters=200,
+                                      init_temp=1., reg_const=1e-4)
  
-        print("SAMPLED ADV PROMPT: " + sampled_prompt)
+        #print("MASKED PROMPT: " + sampled_prompt)
         tokens = torch.tensor(tokenizer.encode(sampled_prompt)).unsqueeze(0).to(device)
         model_output = model(tokens)
-        print("Prediction: " + ("safe" if model_output[0].argmax().item() == 1 else "harmful"))
+        print("Masked Prediction: " + ("safe" if model_output[0].argmax().item() == 1 else "harmful"))
+        #print(model_output[0])
         print()
-        break
